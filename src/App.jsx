e474bae3f2411b7ff609602dc07f0393489c8270
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-11-02 03:16:48 +0100

    init: setup app pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import GlobalStyles from "./styles/GlobalStyles";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Cabins from "./pages/Cabins";
+import NewUsers from "./pages/Users";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Settings from "./pages/Settings";
+import Dashboard from "./pages/Dashboard";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<div>
-				<h1>Hello Oasis</h1>
-				<p>Welcome to world of infinite possibilities</p>
-			</div>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						index
+						element={<Navigate replace to="dashboard" />}
+					/>
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="bookings" element={<Bookings />} />
+					<Route path="cabins" element={<Cabins />} />
+					<Route path="users" element={<NewUsers />} />
+					<Route path="settings" element={<Settings />} />
+					<Route path="login" element={<Login />} />
+					<Route path="account" element={<Account />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 };
