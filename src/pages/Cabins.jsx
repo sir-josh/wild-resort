2e4a92e58f4ee2f0708b339cd688d393f5d05a6d
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-11-03 20:46:32 +0100

    feat: fetch cabin data from supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,26 +1,18 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable.jsx";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins.js";
 
 function Cabins() {
-	useEffect(function () {
-		getCabins().then((data) => console.log(data));
-	}, []);
 	return (
-		<Row type="horizontal">
+		<>
 			<Row type="horizontal">
 				<Heading as="h1">All cabins</Heading>
-				<p>TEST</p>
+				<p>Filter | Sort</p>
 			</Row>
-			<Row type="horizontal">
-				<img
-					src="https://qujwpovtobfewqhbslpa.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
-					alt="Cabin pics"
-					width={800}
-				/>
+			<Row>
+				<CabinTable />
 			</Row>
-		</Row>
+		</>
 	);
 }
 
