b358199440304734c930aea1284211e51d8ab3fc
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-11-04 16:05:00 +0100

    feat: handle form errors

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,53 +1,23 @@
-import styled from "styled-components";
-
-import Input from "../../ui/Input";
-import Form from "../../ui/Form";
-import Button from "../../ui/Button";
-import FileInput from "../../ui/FileInput";
-import Textarea from "../../ui/Textarea";
-import { useForm } from "react-hook-form";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createCabin } from "../../services/apiCabins.js";
 import toast from "react-hot-toast";
+import { useForm } from "react-hook-form";
+import { createCabin } from "../../services/apiCabins.js";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const FormRow = styled.div`
-	display: grid;
-	align-items: center;
-	grid-template-columns: 24rem 1fr 1.2fr;
-	gap: 2.4rem;
-
-	padding: 1.2rem 0;
-
-	&:first-child {
-		padding-top: 0;
-	}
-
-	&:last-child {
-		padding-bottom: 0;
-	}
-
-	&:not(:last-child) {
-		border-bottom: 1px solid var(--color-grey-100);
-	}
-
-	&:has(button) {
-		display: flex;
-		justify-content: flex-end;
-		gap: 1.2rem;
-	}
-`;
-
-const Label = styled.label`
-	font-weight: 500;
-`;
-
-const Error = styled.span`
-	font-size: 1.4rem;
-	color: var(--color-red-700);
-`;
+import Form from "../../ui/Form";
+import Input from "../../ui/Input";
+import Button from "../../ui/Button";
+import Textarea from "../../ui/Textarea";
+import FileInput from "../../ui/FileInput";
+import FormRow from "../../ui/FormRow.jsx";
 
 function CreateCabinForm() {
-	const { register, handleSubmit, reset } = useForm();
+	const {
+		register,
+		handleSubmit,
+		reset,
+		getValues,
+		formState: { errors },
+	} = useForm();
 	const queryClient = useQueryClient();
 
 	const { mutate, isLoading: isCreating } = useMutation({
@@ -66,53 +36,79 @@ function CreateCabinForm() {
 		mutate(data);
 	}
 
+	function onError(errors) {
+		// console.log(errors);
+		// Author decided to use Error handling from useForm() hook <react-hook-form>
+	}
+
 	return (
-		<Form onSubmit={handleSubmit(onSubmit)}>
-			<FormRow>
-				<Label htmlFor="name">Cabin name</Label>
-				<Input type="text" id="name" {...register("name")} />
+		<Form onSubmit={handleSubmit(onSubmit, onError)}>
+			<FormRow label="Cabin name" error={errors?.name?.message}>
+				<Input
+					type="text"
+					id="name"
+					{...register("name", {
+						required: "This field is required",
+					})}
+				/>
 			</FormRow>
 
-			<FormRow>
-				<Label htmlFor="maxCapacity">Maximum capacity</Label>
+			<FormRow
+				label="Maximum capacity"
+				error={errors?.maxCapacity?.message}>
 				<Input
 					type="number"
 					id="maxCapacity"
-					{...register("maxCapacity")}
+					{...register("maxCapacity", {
+						required: "This field is required",
+						min: {
+							value: 1,
+							message: "Capacity should be at least 1",
+						},
+					})}
 				/>
 			</FormRow>
 
-			<FormRow>
-				<Label htmlFor="regularPrice">Regular price</Label>
+			<FormRow
+				label="Regular price"
+				error={errors?.regularPrice?.message}>
 				<Input
 					type="number"
 					id="regularPrice"
-					{...register("regularPrice")}
+					{...register("regularPrice", {
+						required: "This field is required",
+					})}
 				/>
 			</FormRow>
 
-			<FormRow>
-				<Label htmlFor="discount">Discount</Label>
+			<FormRow label="Discount" error={errors?.discount?.message}>
 				<Input
 					type="number"
 					id="discount"
 					defaultValue={0}
-					{...register("discount")}
+					{...register("discount", {
+						required: "This field is required",
+						validate: (value) =>
+							value < getValues().regularPrice ||
+							"Discount should be less than regular price",
+					})}
 				/>
 			</FormRow>
 
-			<FormRow>
-				<Label htmlFor="description">Description for website</Label>
+			<FormRow
+				label="Description on the website"
+				error={errors?.description?.message}>
 				<Textarea
 					type="number"
 					id="description"
 					defaultValue=""
-					{...register("description")}
+					{...register("description", {
+						required: "This field is required",
+					})}
 				/>
 			</FormRow>
 
-			<FormRow>
-				<Label htmlFor="image">Cabin photo</Label>
+			<FormRow label="Cabin photo">
 				<FileInput id="image" accept="image/*" />
 			</FormRow>
 
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -15,6 +15,7 @@ const AppLayout = () => {
 	const Main = styled.main`
 		background-color: var(--color-grey-50);
 		padding: 4rem 4.8rem 6.4rem;
+        overflow: scroll;
 	`;
 
 	const Container = styled.div`
