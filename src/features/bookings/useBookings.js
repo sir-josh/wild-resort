3e359811df0bcebd723c7643b756b2e9ea53ffbb
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-11-11 01:30:40 +0100

    feat: implement API-side filtering on Bookings component

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllBookings } from "../../services/apiBookings.js";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+	const [searchParams] = useSearchParams();
+
+	//FILTER
+	const filterValue = searchParams.get("status");
+	const filter =
+		!filterValue || filterValue === "all"
+			? null
+			: { field: "status", value: filterValue };
+
 	const {
 		isLoading,
 		data: bookings, //eslint-disable-next-line
 		error,
 	} = useQuery({
-		queryKey: ["bookings"],
-		queryFn: getAllBookings,
+		queryKey: ["bookings", filter],
+		queryFn: () => getAllBookings({ filter }),
 	});
 
 	return { isLoading, bookings, error };
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,3 +1,4 @@
+import BookingFilterAndSort from "../features/bookings/BookingFilterAndSort.jsx";
 import BookingTable from "../features/bookings/BookingTable.jsx";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
@@ -8,7 +9,7 @@ function Bookings() {
 		<>
 			<Row type="horizontal">
 				<Heading as="h1">All bookings</Heading>
-				<p>TEST</p>
+				<BookingFilterAndSort />
 			</Row>
 			<BookingTable />
 		</>
