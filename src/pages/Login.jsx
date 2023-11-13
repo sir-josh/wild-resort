e527cb5f1132c8b3ccd691f20e3227b2bf898e68
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-11-13 04:40:28 +0100

    feat: build login page component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import LoginForm from "../features/authentication/LoginForm.jsx";
+import Logo from "../ui/Logo.jsx";
+import Heading from "../ui/Heading.jsx";
 
 const LoginLayout = styled.main`
 	min-height: 100vh;
@@ -8,10 +11,17 @@ const LoginLayout = styled.main`
 	justify-content: center;
 	gap: 3.2rem;
 	background-color: var(--color-grey-50);
+	transform: translateY(-10%);
 `;
 
 function Login() {
-	return <LoginLayout>Login</LoginLayout>;
+	return (
+		<LoginLayout>
+			<Logo />
+			<Heading as="h4">Login to your account</Heading>
+			<LoginForm />
+		</LoginLayout>
+	);
 }
 
 export default Login;
