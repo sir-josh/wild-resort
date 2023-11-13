e9fedccc40ee67d684d42797600f6b06c7d710fd
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-11-13 14:35:17 +0100

    feat: authenticating user login with Supabase

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth.js";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+	const navigate = useNavigate();
+
+	const { mutate: login, isLoading } = useMutation({
+		mutationFn: ({ email, password }) => loginApi({ email, password }),
+		onSuccess: (user) => {
+			navigate("/dashboard");
+		},
+		onError: (err) => {
+			console.log("LOGIN ERROR", err);
+			toast.error("Invalid login credentials");
+		},
+	});
+
+	return { login, isLoading };
+}
