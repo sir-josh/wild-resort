ee8b994679febcb39bfac2af86aa7a64bb7a1afe
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-11-14 17:52:28 +0100

    feat: add dark-mode feature to the app

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Booking from "./pages/Booking.jsx";
 import Checkin from "./pages/Checkin.jsx";
 import PageNotFound from "./pages/PageNotFound";
 import ProtectedRoute from "./ui/ProtectedRoute.jsx";
+import { DarkModeProvider } from "./context/DarkModeContext.jsx";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -29,61 +30,63 @@ const queryClient = new QueryClient({
 
 const App = () => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<ReactQueryDevtools />
-			<GlobalStyles />
-			<BrowserRouter>
-				<Routes>
-					<Route
-						element={
-							<ProtectedRoute>
-								<AppLayout />
-							</ProtectedRoute>
-						}>
+		<DarkModeProvider>
+			<QueryClientProvider client={queryClient}>
+				<ReactQueryDevtools />
+				<GlobalStyles />
+				<BrowserRouter>
+					<Routes>
 						<Route
-							index
-							element={<Navigate replace to="dashboard" />}
-						/>
-						<Route path="dashboard" element={<Dashboard />} />
-						<Route path="bookings" element={<Bookings />} />
-						<Route
-							path="bookings/:bookingId"
-							element={<Booking />}
-						/>
-						<Route
-							path="checkin/:bookingId"
-							element={<Checkin />}
-						/>
-						<Route path="cabins" element={<Cabins />} />
-						<Route path="users" element={<NewUsers />} />
-						<Route path="settings" element={<Settings />} />
-						<Route path="account" element={<Account />} />
-					</Route>
-					<Route path="login" element={<Login />} />
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-			<Toaster
-				position="top-right"
-				gutter={12}
-				containerStyle={{ margin: "8px" }}
-				toastOptions={{
-					success: {
-						duration: 3000,
-					},
-					error: {
-						duration: 5000,
-					},
-					style: {
-						fontSize: "16px",
-						maxWidth: "400px",
-						padding: "16px 24px",
-						backgroundColor: "var(--color-grey-400)",
-						color: "var(--color-grey-700)",
-					},
-				}}
-			/>
-		</QueryClientProvider>
+							element={
+								<ProtectedRoute>
+									<AppLayout />
+								</ProtectedRoute>
+							}>
+							<Route
+								index
+								element={<Navigate replace to="dashboard" />}
+							/>
+							<Route path="dashboard" element={<Dashboard />} />
+							<Route path="bookings" element={<Bookings />} />
+							<Route
+								path="bookings/:bookingId"
+								element={<Booking />}
+							/>
+							<Route
+								path="checkin/:bookingId"
+								element={<Checkin />}
+							/>
+							<Route path="cabins" element={<Cabins />} />
+							<Route path="users" element={<NewUsers />} />
+							<Route path="settings" element={<Settings />} />
+							<Route path="account" element={<Account />} />
+						</Route>
+						<Route path="login" element={<Login />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
+				<Toaster
+					position="top-right"
+					gutter={12}
+					containerStyle={{ margin: "8px" }}
+					toastOptions={{
+						success: {
+							duration: 3000,
+						},
+						error: {
+							duration: 5000,
+						},
+						style: {
+							fontSize: "16px",
+							maxWidth: "400px",
+							padding: "16px 24px",
+							backgroundColor: "var(--color-grey-400)",
+							color: "var(--color-grey-700)",
+						},
+					}}
+				/>
+			</QueryClientProvider>
+		</DarkModeProvider>
 	);
 };
 
---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,15 @@
+import { useDarkMode } from "../context/DarkModeContext.jsx";
+import ButtonIcon from "./ButtonIcon.jsx";
+import { HiOutlineMoon } from "react-icons/hi2";
+import { HiOutlineSun } from "react-icons/hi2";
+
+function DarkModeToggle() {
+	const { isDarkMode, toggleDarkMode } = useDarkMode();
+	return (
+		<ButtonIcon onClick={toggleDarkMode}>
+			{isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
+		</ButtonIcon>
+	);
+}
+
+export default DarkModeToggle;
