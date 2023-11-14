e7a821f4be544e802b4926784e96247fcedbdaf8
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2023-11-14 03:16:19 +0100

    feat: build the app header

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -23,11 +23,15 @@ const Avatar = styled.img`
 
 function UserAvatar() {
 	const { user } = useUser();
+	const { fullName, avatar } = user.user_metadata;
 
 	return (
 		<StyledUserAvatar>
-			{user.email}
-			<Avatar />
+			<Avatar
+				src={avatar || "default-user.jpg"}
+				alt={`Avatar of ${fullName}`}
+			/>
+			<span> {fullName.split(" ")[0]}</span>
 		</StyledUserAvatar>
 	);
 }
